d4e3040fb7ca91eb9f6e9fcfc917486325ff1347
unknown <cmac@arquitecturajava.com>
2023-05-10 10:05:01 +0200

    añadiendo anotaciones Requestmapping a CompaniesController

--- src/main/java/es/logixs/web/controllers/CompaniesController.java
+++ src/main/java/es/logixs/web/controllers/CompaniesController.java
@@ -1,8 +1,25 @@
 package es.logixs.web.controllers;
 
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import es.logixs.web.domain.Companies;
+import es.logixs.web.services.UsersCompanyService;
+
 @RestController
+@RequestMapping("webapi/companies")
 public class CompaniesController {
     
+    @Autowired
+    private UsersCompanyService servicioUserCompany;
+
+    @GetMapping
+    public List<Companies> findAllCompanies() {
+        return servicioUserCompany.findAllCompanies();
+    }
+
 }
