9a84c13c99a799f84fecad3d6da73d4caa23cbc9
Romer Enrique Alvarez Martinez <romerenriquealvarezmartinez@Romers-MacBook-Pro.local>
2023-05-10 13:19:12 +0100

    add products repository to springboot

---
+++ src/main/java/es/logixs/web/repositories/ProductsRepository.java
@@ -0,0 +1,14 @@
+package es.logixs.web.repositories;
+
+
+import es.logixs.web.domain.Products;
+
+import java.util.List;
+
+public interface ProductsRepository {
+
+    Products insert(Products product);
+    Products findOne(String id);
+    List<Products> findAll();
+    void delete(String id);
+}
