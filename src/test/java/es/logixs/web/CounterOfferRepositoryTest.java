cfa9077b0b99adf69730b4dfc8db4c88b7b2b8e1
Fero <ferorock44@gmail.com>
2023-05-11 12:11:32 +0100

    :white_check_mark: Add findOne CounterOffer Test

--- src/test/java/es/logixs/web/CounterOfferRepositoryTest.java
+++ src/test/java/es/logixs/web/CounterOfferRepositoryTest.java
@@ -1,12 +1,17 @@
 package es.logixs.web;
 
+import es.logixs.web.domain.CounterOffer;
 import es.logixs.web.repositories.CounterOfferRepository;
 import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.jdbc.Sql;
 
 import java.io.IOException;
+import java.util.List;
+
+import static org.junit.jupiter.api.Assertions.*;
 
 @SpringBootTest
 @Sql({ "classpath:schemacounteroffer.sql", "classpath:datacounteroffer.sql" })
@@ -22,6 +27,14 @@ class CounterOfferRepositoryTest {
 
 
     }
+
+    @Test
+    void findOne() {
+        CounterOffer counterOfferFinal= counterOfferRepository.findOne("1A");
+
+        assertEquals("name1", counterOfferFinal.getName());
+    }
+
 
 
 }
