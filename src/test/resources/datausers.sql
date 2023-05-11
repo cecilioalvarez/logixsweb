ae86546dc25ed8a43f58493a4ac965c9f56cc887
unknown <cmac@arquitecturajava.com>
2023-05-11 10:08:17 +0200

    refactoring script carga database y sus carpetas para user

---
+++ src/test/resources/datausers.sql
@@ -0,0 +1,2 @@
+insert into user(objectId, name, lastName, email) values ('2A', 'Pedro', 'Sanchez', 'pedro@mail.com');
+insert into user(objectId, name, lastName, email) values ('1A', 'Maria', 'Sanchez', 'maria@mail.com');
---
+++ src/test/resources/schemausers.sql
@@ -0,0 +1,7 @@
+DROP TABLE IF EXISTS user;
+CREATE TABLE user (
+  objectId varchar(25) DEFAULT NULL,
+  name varchar(50) DEFAULT NULL,
+  lastName varchar(50) DEFAULT NULL,
+  email varchar(50) DEFAULT NULL
+); 
