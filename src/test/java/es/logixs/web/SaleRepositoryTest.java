fbcf155278bdcd515118756041a3682b9e554858
Fero <ferorock44@gmail.com>
2023-05-11 16:21:39 +0100

    :sparkles: Create SaleRepositoryTest

---
+++ src/test/java/es/logixs/web/SaleRepositoryTest.java
@@ -0,0 +1,32 @@
+package es.logixs.web;
+
+import es.logixs.web.domain.Sale;
+import es.logixs.web.repositories.SaleRepository;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.jdbc.Sql;
+
+import java.io.IOException;
+import java.util.List;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+@SpringBootTest
+@Sql({ "classpath:schemasale.sql", "classpath:datasale.sql" })
+class SaleRepositoryTest {
+
+    @Autowired
+    private SaleRepository saleRepository;
+
+
+
+    @BeforeEach
+    public void inicializar() throws IOException {
+
+
+    }
+
+
+}
