7120de3ac370437f4c61206fbc6766a168f3cbd4
Fero <ferorock44@gmail.com>
2023-05-12 12:53:16 +0100

    :white_check_mark: Add insertCounterOffer in CounterOfferControllerTest

--- src/test/java/es/logixs/web/controllers/CounterOfferControllerTest.java
+++ src/test/java/es/logixs/web/controllers/CounterOfferControllerTest.java
@@ -8,14 +8,15 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.http.MediaType;
 import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.MvcResult;
 
 import java.util.List;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.mockito.Mockito.*;
-import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.delete;
-import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 @SpringBootTest
@@ -70,5 +71,22 @@ class CounterOfferControllerTest {
         mvc.perform(delete("/webapi/counteroffer/1A")).andExpect(status().isOk());
 
         verify(offerCounterofferService, times(1)).deleteCounterOffer(counterOffer);
+    }
+
+    @Test
+    void insertCounterOffer() throws Exception {
+        CounterOffer counterOffer = new CounterOffer("1A", "name1", "vom1", 2.0, 4.0, 10.0);
+
+        when(offerCounterofferService.insertCounterOffer(counterOffer)).thenReturn(counterOffer);
+        MvcResult result = mvc.perform(post("/webapi/counteroffer")
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .content(objectMapper.writeValueAsString(counterOffer))
+                        .accept(MediaType.APPLICATION_JSON))
+                .andExpect(status().isOk()).andReturn();
+        String responseJson = result.getResponse().getContentAsString();
+        CounterOffer createdCounterOffer = objectMapper.readValue(responseJson, CounterOffer.class);
+
+        assertEquals(counterOffer, createdCounterOffer);
+        verify(offerCounterofferService, times(1)).insertCounterOffer(counterOffer);
     }
 }
