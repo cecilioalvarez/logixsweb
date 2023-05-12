f563430aaa8c58d72f82af62717f59c6aecb2e0f
unknown <cmac@arquitecturajava.com>
2023-05-12 11:56:17 +0200

    UserControllers Test POST y DELETE

--- src/test/java/es/logixs/web/controllers/UserControllerTest.java
+++ src/test/java/es/logixs/web/controllers/UserControllerTest.java
@@ -1,17 +1,23 @@
 package es.logixs.web.controllers;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.mockito.Mockito.times;
+import static org.mockito.Mockito.verify;
 import static org.mockito.Mockito.when;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.delete;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 import java.util.List;
+
 
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.http.MediaType;
 import org.springframework.test.web.servlet.MockMvc;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
@@ -26,7 +32,8 @@ class UserControllerTest {
     // que hace peticiones http a traves de los objectos mock
     @Autowired
     private MockMvc mvc;
-    @Autowired ObjectMapper objectMapper;
+    @Autowired
+    ObjectMapper objectMapper;
     @MockBean
     private UserCompanyService servicio;
 
@@ -37,46 +44,78 @@ class UserControllerTest {
         User user2 = new User("2B", "ana", "gomez", "ana@gmail.com");
         User user3 = new User("3C", "gema", "sanchez", "gema@gmail.com");
 
-        List<User> userList=List.of(user1, user2, user3);
+        List<User> userList = List.of(user1, user2, user3);
         when(servicio.findAllUsers()).thenReturn(userList);
 
-        String listExpected= objectMapper.writeValueAsString(userList);
+        String listExpected = objectMapper.writeValueAsString(userList);
         // test de risa
-        String userListResultJSON=mvc.perform(get("/webapi/user")).andExpect(status().isOk()).andExpect(status().isOk()).andReturn().getResponse().getContentAsString();
+        String userListResultJSON = mvc.perform(get("/webapi/user")).andExpect(status().isOk())
+                .andExpect(status().isOk()).andReturn().getResponse().getContentAsString();
 
-        assertEquals(listExpected,userListResultJSON);
+        assertEquals(listExpected, userListResultJSON);
     }
 
     @Test
     void findOneUserTest() throws Exception {
 
         User user1 = new User("1A", "pedro", "perez", "pedro@gmail.com");
-    
 
         when(servicio.findOneUser("1A")).thenReturn(user1);
         // test de risa
-        String userJsonResult=mvc.perform(get("/webapi/user/1A")).andExpect(status().isOk()).andReturn().getResponse().getContentAsString();
+        String userJsonResult = mvc.perform(get("/webapi/user/1A")).andExpect(status().isOk()).andReturn().getResponse()
+                .getContentAsString();
 
-        String userExpected= objectMapper.writeValueAsString(user1);
+        String userExpected = objectMapper.writeValueAsString(user1);
 
-        assertEquals(userExpected,userJsonResult);
+        assertEquals(userExpected, userJsonResult);
     }
 
     @Test
-    void findOneUser2Test() throws Exception {
+    void deleteUserTest() throws Exception {
 
-        User user1 = new User("1A", "pedro", "perez", "pedro@gmail.com");
-    
+        User user1 = new User("1A");
 
-        when(servicio.findOneUser("1A")).thenReturn(user1);
+        // verify(servicio,times(1)).deleteUser(user1);
         // test de risa
-        String userJsonResult=mvc.perform(get("/webapi/user/1A")).andExpect(status().isOk()).andReturn().getResponse().getContentAsString();
+        mvc.perform(delete("/webapi/user/1A")).andExpect(status().isOk());
 
-        String userExpected= "{\"objectId\":\"1A\",\"name\":\"pedro\",\"lastName\":\"perez\",\"email\":\"pedro@gmail.com\"}";
-
-        assertEquals(userExpected,userJsonResult);
     }
 
+    @Test
+    void insertUserTest() throws Exception {
 
+        User user1 = new User("10A","juan","gomez","juan@gmail.com");
+
+        // verify(servicio,times(1)).deleteUser(user1);
+        // test de risa
+
+        when(servicio.insertUser(user1)).thenReturn(user1);
+        mvc.perform(post("/webapi/user")
+        .contentType(MediaType.APPLICATION_JSON)
+        .content( objectMapper.writeValueAsString(user1))
+        .accept(MediaType.APPLICATION_JSON))
+        .andExpect(status().isOk());
+
+    }
+
+    /*
+     * @Test
+     * void findOneUser2Test() throws Exception {
+     * 
+     * User user1 = new User("1A", "pedro", "perez", "pedro@gmail.com");
+     * 
+     * 
+     * when(servicio.findOneUser("1A")).thenReturn(user1);
+     * // test de risa
+     * String
+     * userJsonResult=mvc.perform(get("/webapi/user/1A")).andExpect(status().isOk())
+     * .andReturn().getResponse().getContentAsString();
+     * 
+     * String userExpected=
+     * "{\"objectId\":\"1A\",\"name\":\"pedro\",\"lastName\":\"perez\",\"email\":\"pedro@gmail.com\"}";
+     * 
+     * assertEquals(userExpected,userJsonResult);
+     * }
+     */
 
 }
