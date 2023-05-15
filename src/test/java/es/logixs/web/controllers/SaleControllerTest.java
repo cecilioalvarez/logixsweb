90c99fe4b478d97c62283c694db70b40e85d9eec
Fero <ferorock44@gmail.com>
2023-05-15 14:25:36 +0100

    :white_check_mark: Add update Sale in SaleControllerTest

--- src/test/java/es/logixs/web/controllers/SaleControllerTest.java
+++ src/test/java/es/logixs/web/controllers/SaleControllerTest.java
@@ -1,17 +1,6 @@
 package es.logixs.web.controllers;
 
-import static org.junit.jupiter.api.Assertions.assertEquals;
-import static org.mockito.Mockito.times;
-import static org.mockito.Mockito.verify;
-import static org.mockito.Mockito.when;
-import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
-import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
-import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.delete;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
-
-import java.util.List;
-
-
+import com.fasterxml.jackson.databind.ObjectMapper;
 import es.logixs.web.domain.Sale;
 import es.logixs.web.services.SaleProductRequestService;
 import org.junit.jupiter.api.Test;
@@ -21,81 +10,105 @@ import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.http.MediaType;
 import org.springframework.test.web.servlet.MockMvc;
-
-import com.fasterxml.jackson.databind.ObjectMapper;
-
-
 import org.springframework.test.web.servlet.MvcResult;
+
+import java.util.List;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.mockito.Mockito.*;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 @SpringBootTest
 @AutoConfigureMockMvc
 public class SaleControllerTest {
-  @Autowired
-  private MockMvc mvc;
+    @Autowired
+    private MockMvc mvc;
 
-  @Autowired
-  ObjectMapper objectMapper;
+    @Autowired
+    ObjectMapper objectMapper;
 
-  @MockBean
-  private SaleProductRequestService service;
+    @MockBean
+    private SaleProductRequestService service;
 
-  @Test
-  void findAllSalesTest() throws Exception {
-    Sale sale1 = new Sale("1A", "1B", "1B","aaa", "aaa","aaa", false);
-    Sale sale2 = new Sale("2A", "2B", "2B","bbb", "bbb","bbb", false);
-    Sale sale3 = new Sale("3A", "3B", "3B","ccc", "ccc","ccc", false);
+    @Test
+    void findAllSalesTest() throws Exception {
+        Sale sale1 = new Sale("1A", "1B", "1B", "aaa", "aaa", "aaa", false);
+        Sale sale2 = new Sale("2A", "2B", "2B", "bbb", "bbb", "bbb", false);
+        Sale sale3 = new Sale("3A", "3B", "3B", "ccc", "ccc", "ccc", false);
 
-    List<Sale> saleList = List.of(sale1, sale2, sale3);
-    when(service.findAllSales()).thenReturn(saleList);
+        List<Sale> saleList = List.of(sale1, sale2, sale3);
+        when(service.findAllSales()).thenReturn(saleList);
 
-    String listExpected = objectMapper.writeValueAsString(saleList);
-    // test de risa
-    String saleListResultJSON = mvc.perform(get("/webapi/sale")).andExpect(status().isOk())
-            .andExpect(status().isOk()).andReturn().getResponse().getContentAsString();
+        String listExpected = objectMapper.writeValueAsString(saleList);
+        // test de risa
+        String saleListResultJSON = mvc.perform(get("/webapi/sale")).andExpect(status().isOk())
+                .andExpect(status().isOk()).andReturn().getResponse().getContentAsString();
 
-    assertEquals(listExpected, saleListResultJSON);
-  }
-  @Test
-  void findOneSaleTest() throws Exception {
-    Sale sale1 = new Sale("1A", "1B", "1B","aaa", "aaa","aaa", false);
+        assertEquals(listExpected, saleListResultJSON);
+    }
 
-    when(service.findOneSale("1A")).thenReturn(sale1);
+    @Test
+    void findOneSaleTest() throws Exception {
+        Sale sale1 = new Sale("1A", "1B", "1B", "aaa", "aaa", "aaa", false);
 
-    String saleExpected = objectMapper.writeValueAsString(sale1);
-    // test de risa
-    String saleResultJSON = mvc.perform(get("/webapi/sale/1A")).andExpect(status().isOk())
-           .andExpect(status().isOk()).andReturn().getResponse().getContentAsString();
+        when(service.findOneSale("1A")).thenReturn(sale1);
 
-    assertEquals(saleExpected, saleResultJSON);
-  }
+        String saleExpected = objectMapper.writeValueAsString(sale1);
+        // test de risa
+        String saleResultJSON = mvc.perform(get("/webapi/sale/1A")).andExpect(status().isOk())
+                .andExpect(status().isOk()).andReturn().getResponse().getContentAsString();
 
-  @Test
-  void deleteSaleTest() throws Exception {
-    Sale saleToDelete = new Sale("1A", "1B", "1B","aaa", "aaa","aaa", false);
+        assertEquals(saleExpected, saleResultJSON);
+    }
 
-    when(service.findOneSale("1A")).thenReturn(saleToDelete);
+    @Test
+    void deleteSaleTest() throws Exception {
+        Sale saleToDelete = new Sale("1A", "1B", "1B", "aaa", "aaa", "aaa", false);
 
-    mvc.perform(delete("/webapi/sale/1A"))
-            .andExpect(status().isOk());
+        when(service.findOneSale("1A")).thenReturn(saleToDelete);
 
-    verify(service, times(1)).deleteSale("1A");
-  }
+        mvc.perform(delete("/webapi/sale/1A"))
+                .andExpect(status().isOk());
 
-  @Test
-  void insertSaleTest() throws Exception {
-    Sale sale = new Sale("1A", "1B", "1B", "aaa", "aaa", "aaa", false);
+        verify(service, times(1)).deleteSale("1A");
+    }
 
-    when(service.insertSale(sale)).thenReturn(sale);
-    MvcResult result = mvc.perform(post("/webapi/sale")
-            .contentType(MediaType.APPLICATION_JSON)
-            .content( objectMapper.writeValueAsString(sale))
-            .accept(MediaType.APPLICATION_JSON))
-            .andExpect(status().isOk()).andReturn();
+    @Test
+    void insertSaleTest() throws Exception {
+        Sale sale = new Sale("1A", "1B", "1B", "aaa", "aaa", "aaa", false);
 
-    String responseJSON = result.getResponse().getContentAsString();
-    Sale createdSale = objectMapper.readValue(responseJSON, Sale.class);
-    assertEquals(sale, createdSale);
+        when(service.insertSale(sale)).thenReturn(sale);
+        MvcResult result = mvc.perform(post("/webapi/sale")
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .content(objectMapper.writeValueAsString(sale))
+                        .accept(MediaType.APPLICATION_JSON))
+                .andExpect(status().isOk()).andReturn();
 
-  verify(service, times(1)).insertSale(sale);
-  }
+        String responseJSON = result.getResponse().getContentAsString();
+        Sale createdSale = objectMapper.readValue(responseJSON, Sale.class);
+        assertEquals(sale, createdSale);
+
+        verify(service, times(1)).insertSale(sale);
+    }
+
+    @Test
+    void updateSale() throws Exception {
+        Sale saleToUpdate = new Sale("1A", "1B", "1B", "aaa", "aaa", "aaa", false);
+        saleToUpdate.setCode("bbb");
+        saleToUpdate.setClientId("1A");
+
+        when(service.findOneSale("1A")).thenReturn(saleToUpdate);
+        service.updateSale(saleToUpdate, "1A");
+        mvc.perform(put("/webapi/sale/1A")
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .content(objectMapper.writeValueAsString(saleToUpdate))
+                        .accept(MediaType.APPLICATION_JSON))
+                .andExpect(status().isOk());
+
+        Sale saleUpdated = service.findOneSale("1A");
+
+        assertEquals(saleToUpdate.getCode(), saleUpdated.getCode());
+        assertEquals(saleToUpdate.getClientId(), saleUpdated.getClientId());
+    }
 }
