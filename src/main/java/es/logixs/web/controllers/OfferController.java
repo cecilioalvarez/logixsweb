e0419bb42ef3db5f7e88f2fdbd87ba6da8cbbfc9
Eduardo Pérez Suárez <eduperz38@gmail.com>
2023-05-15 13:34:22 +0100

    :pencil2: adapt OfferController with OfferDTO type

--- src/main/java/es/logixs/web/controllers/OfferController.java
+++ src/main/java/es/logixs/web/controllers/OfferController.java
@@ -1,10 +1,12 @@
 package es.logixs.web.controllers;
 
 import es.logixs.web.domain.Offer;
+import es.logixs.web.dto.OfferDTO;
 import es.logixs.web.services.OfferCounterofferService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
+import java.util.ArrayList;
 import java.util.List;
 
 @RestController
@@ -15,27 +17,31 @@ public class OfferController {
     private OfferCounterofferService offerCounterofferService;
     
     @GetMapping()
-    private List<Offer> findAllOffers() {
-        return offerCounterofferService.findAllOffers();
+    private List<OfferDTO> findAllOffers() {
+        List<OfferDTO> listOfferDTO = new ArrayList<>();
+        for (Offer offer: offerCounterofferService.findAllOffers()) {
+            listOfferDTO.add(new OfferDTO(offer));
+        }
+        return listOfferDTO;
     }
     
     @GetMapping("/{objectId}")
-    private Offer findOneOffer(@PathVariable String objectId) {
-        return offerCounterofferService.findOneOffer(objectId);
+    private OfferDTO findOneOffer(@PathVariable String objectId) {
+        return new OfferDTO(offerCounterofferService.findOneOffer(objectId));
     }
 
     @DeleteMapping("/{objectId}")
     public void deleteOffer(@PathVariable String objectId) {
         offerCounterofferService.deleteOffer(objectId);
     }
+
     @PostMapping
-    public Offer insertOffer(@RequestBody Offer sale) {
-        return offerCounterofferService.insertOffer(sale);
+    public OfferDTO insertOffer(@RequestBody OfferDTO offerDTO) {
+        return new OfferDTO(offerCounterofferService.insertOffer(offerDTO.createOffer()));
     }
+
     @PutMapping("/{objectId}")
-    public void updateOffer(@RequestBody Offer sale,@PathVariable String objectId) {
-        offerCounterofferService.updateOffer(sale,objectId);
+    public void updateOffer(@RequestBody OfferDTO offerDTO,@PathVariable String objectId){
+        offerCounterofferService.updateOffer(offerDTO.createOffer(),objectId);
     }
-    
-    
 }
