d2212c8b0bbda399f9570a5bf870c5e1285cdc00
Eduardo Diez (PWC) <eduardo.diez.montenegro@pwc.com>
2023-05-16 17:04:18 +0100

    Add getter and setters for the new attributes

--- src/main/java/es/logixs/web/repositories/mysql/CompanyRepositoryMySQL.java
+++ src/main/java/es/logixs/web/repositories/mysql/CompanyRepositoryMySQL.java
@@ -13,18 +13,34 @@ import es.logixs.web.repositories.mysql.mappers.CompanyMapper;
 @Repository
 public class CompanyRepositoryMySQL implements CompanyRepository {
 
-    private final static String sqlInsert = "insert into company (objectId,code,licenseId,name,taxId) values (?,?,?,?,?)";
+    private final static String sqlInsert = "insert into company (objectId,code,state,licenseId,limitUsers,name,address,phone," +
+            "countryIso, taxId, url, createdAt, updatedAt) values (?,?,?,?,?,?,?,?,?,?,?,?,?)";
     private final static String sqlFindAll = "select * from company";
     private final static String sqlDelete = "delete from company where objectId=?";
     private final static String sqlFindOne = "select * from company where objectId=?";
-    private final static String sqlUpdate = "update company set objectId=?,code=? , licenseId=? , name=? ,taxId=? where objectId=?";
+    private final static String sqlUpdate = "update company set objectId=?, code=?, state=?, licenseId=?, limitUsers=?, name=?, address=?, " +
+            "taxId=?, url=?, createdAt=?, updatedAt=? where objectId=?";
   
     @Autowired
     private JdbcTemplate plantilla;
 
     @Override
     public Company insert(Company company) {
-        plantilla.update(sqlInsert, company.getObjectId(),company.getCode(), company.getLicenseId(),company.getName(), company.getTaxId());
+        plantilla.update(
+                sqlInsert,
+                company.getObjectId(),
+                company.getCode(),
+                company.getState(),
+                company.getLicenseId(),
+                company.getLimitUsers(),
+                company.getName(),
+                company.getAddress(),
+                company.getPhone(),
+                company.getCountryIso(),
+                company.getTaxId(),
+                company.getUrl(),
+                company.getCreateAt(),
+                company.getUpdateAt());
         return company;
     }
     @Override
@@ -39,19 +55,48 @@ public class CompanyRepositoryMySQL implements CompanyRepository {
     
     @Override
     public Company findOne(String objectId) {
-       
+
        return  plantilla.queryForObject(sqlFindOne, new CompanyMapper(),objectId);
     }
     @Override
     public void update(Company company) {
       
-        plantilla.update(sqlUpdate,company.getObjectId(), company.getCode(), company.getLicenseId(),company.getName(), company.getTaxId(),company.getObjectId());
-
+        plantilla.update(
+                sqlUpdate,
+                company.getObjectId(),
+                company.getCode(),
+                company.getState(),
+                company.getLicenseId(),
+                company.getLimitUsers(),
+                company.getName(),
+                company.getAddress(),
+                company.getPhone(),
+                company.getCountryIso(),
+                company.getTaxId(),
+                company.getUrl(),
+                company.getCreateAt(),
+                company.getUpdateAt(),
+                company.getObjectId());
     }
     @Override
     public void update(Company company, Company oldCompany) {
         
-        plantilla.update(sqlUpdate,company.getObjectId(), company.getCode(), company.getLicenseId(),company.getName(), company.getTaxId(),oldCompany.getObjectId());
+        plantilla.update(
+                sqlUpdate,
+                company.getObjectId(),
+                company.getCode(),
+                company.getState(),
+                company.getLicenseId(),
+                company.getLimitUsers(),
+                company.getName(),
+                company.getAddress(),
+                company.getPhone(),
+                company.getCountryIso(),
+                company.getTaxId(),
+                company.getUrl(),
+                company.getCreateAt(),
+                company.getUpdateAt(),
+                oldCompany.getObjectId());
     }
 
 }
