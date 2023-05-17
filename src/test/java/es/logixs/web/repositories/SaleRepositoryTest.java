b9271aaf65d97c14cb2bbc322072a8a551737287
Marianela Serodino (PWC) <marianela.serodino.runco@pwc.com>
2023-05-17 17:20:03 +0100

    :recycle: Refactor sql statements and SaleRepositoryTest
    > On schemasale.sql
     - Change objectId length from 25 to 255
    
    > On datasale.sql
     - Change objectId to a UUID
    
    > On SaleRepositoryTest
     - Change objectId from String to UUID

--- src/test/java/es/logixs/web/repositories/SaleRepositoryTest.java
+++ src/test/java/es/logixs/web/repositories/SaleRepositoryTest.java
@@ -1,73 +1,91 @@
 package es.logixs.web.repositories;
 
-import static org.junit.jupiter.api.Assertions.assertEquals;
-import static org.junit.jupiter.api.Assertions.assertFalse;
-import static org.junit.jupiter.api.Assertions.assertTrue;
-
-import java.util.List;
-
 import es.logixs.web.domain.Sale;
 import es.logixs.web.repositories.mysql.SaleRepositoryMySQL;
-
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.jdbc.Sql;
 
+import java.util.List;
+import java.util.UUID;
+
+import static org.junit.jupiter.api.Assertions.*;
+
 
 @SpringBootTest
-@Sql({ "classpath:schemasale.sql", "classpath:datasale.sql" })
+@Sql({"classpath:schemasale.sql", "classpath:datasale.sql"})
 class SaleRepositoryTest {
 
-    @Autowired
-    private SaleRepositoryMySQL saleRepository;
+  @Autowired
+  private SaleRepositoryMySQL saleRepositoryMySQL;
+
+  @Test
+  void findOneTest() {
+    Sale saleFinal = saleRepositoryMySQL.findOne(UUID.fromString("391e8a7e-b050-44df-b86f-6718a267d020"));
+
+    assertEquals("1A", saleFinal.getCode());
+  }
+
+  @Test
+  void findAllTest() {
+    List<Sale> saleList = saleRepositoryMySQL.findAll();
+
+    assertTrue(saleList.size() >= 4);
+  }
+
+  @Test
+  void insertTest() {
+    Sale sale = new Sale(UUID.randomUUID(), "0005", "0005", "5A", "5", "5", true);
+
+    Sale saleFinal = saleRepositoryMySQL.insert(sale);
+
+    assertEquals(sale, saleFinal);
+  }
+
+  @Test
+  void deleteTest() {
+    Sale sale = new Sale(UUID.fromString("391e8a7e-b050-44df-b86f-6718a267d020"));
+
+    saleRepositoryMySQL.delete(sale.getObjectId());
+    List<Sale> sales = saleRepositoryMySQL.findAll();
+
+    assertFalse(sales.contains(sale));
+
+  }
+
+  @Test
+  void updateTest() {
+    Sale saleToUpdate = saleRepositoryMySQL.findOne(UUID.fromString("391e8a7e-b050-44df-b86f-6718a267d020"));
+    saleToUpdate.setOwnerId("0005");
+    saleToUpdate.setClientId("0005");
+    saleToUpdate.setCode("5A");
+    saleToUpdate.setOfferId("5");
+    saleToUpdate.setCounterOfferId("5");
+    saleToUpdate.setCounterOffer(false);
+    saleRepositoryMySQL.update(saleToUpdate);
+    Sale saleUpdated = saleRepositoryMySQL.findOne(UUID.fromString("391e8a7e-b050-44df-b86f-6718a267d020"));
+
+    assertEquals("0005", saleUpdated.getOwnerId());
+    assertEquals("0005", saleUpdated.getClientId());
+    assertEquals("5A", saleUpdated.getCode());
+    assertEquals("5", saleUpdated.getOfferId());
+    assertEquals("5", saleUpdated.getCounterOfferId());
+    assertFalse(saleUpdated.isCounterOffer());
+  }
 
     @Test
-    void findOneTest() {
-        Sale saleFinal= saleRepository.findOne("4A");
-
-        assertEquals("4A", saleFinal.getCode());
-    }
-
-    @Test
-    void findAllTest() {
-        List<Sale> saleList = saleRepository.findAll();
-
-        assertTrue(saleList.size()>=4);
-    }
-
-    @Test
-    void insertTest() {
-        Sale sale = new Sale("5A", "0005", "0005", "5A", "5", "5", true);
-
-        Sale saleFinal = saleRepository.insert(sale);
-
-        assertEquals(sale, saleFinal);
-    }
-
-    @Test
-    void deleteTest() {
-        Sale sale = new Sale("5A", "0005", "0005", "5A", "5", "5", false);
-
-        saleRepository.insert(sale);
-        saleRepository.delete("5A");
-        List<Sale> sales = saleRepository.findAll();
-
-        assertFalse(sales.contains(sale));
-
-    }
-
-    @Test
-    void updateTest() {
-        Sale saleToUpdate = saleRepository.findOne("1A");
+    void updateFieldsIDTest() {
+        Sale saleToUpdate = saleRepositoryMySQL.findOne(UUID.fromString("391e8a7e-b050-44df-b86f-6718a267d020"));
+        saleToUpdate.setObjectId(UUID.fromString("391e8a7e-b050-44df-b86f-6718a267d020"));
         saleToUpdate.setOwnerId("0005");
         saleToUpdate.setClientId("0005");
         saleToUpdate.setCode("5A");
         saleToUpdate.setOfferId("5");
         saleToUpdate.setCounterOfferId("5");
         saleToUpdate.setCounterOffer(false);
-        saleRepository.update(saleToUpdate);
-        Sale saleUpdated = saleRepository.findOne("1A");
+        saleRepositoryMySQL.update(saleToUpdate);
+        Sale saleUpdated = saleRepositoryMySQL.findOne(UUID.fromString("391e8a7e-b050-44df-b86f-6718a267d020"));
 
         assertEquals("0005", saleUpdated.getOwnerId());
         assertEquals("0005", saleUpdated.getClientId());
--- src/test/resources/datasale.sql
+++ src/test/resources/datasale.sql
@@ -1,5 +1,5 @@
-insert into sale (objectId,ownerId,clientId,code,offerId,counterOfferId,isCounterOffer) values('1A', '0001', '0001', '1A', '1', '1', true);
-insert into sale (objectId,ownerId,clientId,code,offerId,counterOfferId,isCounterOffer) values('2A', '0002', '0002', '2A', '2', '2', true);
-insert into sale (objectId,ownerId,clientId,code,offerId,counterOfferId,isCounterOffer) values('3A', '0003', '0003', '3A', '3', '3', false);
-insert into sale (objectId,ownerId,clientId,code,offerId,counterOfferId,isCounterOffer) values('4A', '0004', '0004', '4A', '4', '4', false);
+insert into sale (objectId,ownerId,clientId,code,offerId,counterOfferId,isCounterOffer) values('391e8a7e-b050-44df-b86f-6718a267d020', '0001', '0001', '1A', '1', '1', true);
+insert into sale (objectId,ownerId,clientId,code,offerId,counterOfferId,isCounterOffer) values('391e8a7e-b050-44df-b86f-6718a267d021', '0002', '0002', '2A', '2', '2', true);
+insert into sale (objectId,ownerId,clientId,code,offerId,counterOfferId,isCounterOffer) values('391e8a7e-b050-44df-b86f-6718a267d022', '0003', '0003', '3A', '3', '3', false);
+insert into sale (objectId,ownerId,clientId,code,offerId,counterOfferId,isCounterOffer) values('391e8a7e-b050-44df-b86f-6718a267d023', '0004', '0004', '4A', '4', '4', false);
 
