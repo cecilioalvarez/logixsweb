5871d168ec3dfc9cdb895352d89ef8042110ed15
Marianela Serodino (PWC) <marianela.serodino.runco@pwc.com>
2023-05-17 17:15:47 +0100

    :recycle: Refactor SaleRepository and SaleRepositoryMySQL
    > Change objectId type from String to UUID on SaleRepository
    > SaleRepositoryMySQL:
     - Remove SQL queries
     - Add @PersistenceContext and @Transactional annotations
     - Refactor methods using EntityManager

--- src/main/java/es/logixs/web/repositories/SaleRepository.java
+++ src/main/java/es/logixs/web/repositories/SaleRepository.java
@@ -4,14 +4,15 @@ package es.logixs.web.repositories;
 import es.logixs.web.domain.Sale;
 
 import java.util.List;
+import java.util.UUID;
 
 public interface SaleRepository {
 
     Sale insert(Sale sale);
 
-    Sale findOne(String objectId);
+    Sale findOne(UUID objectId);
     List<Sale> findAll();
-    void delete(String objectId);
+    void delete(UUID objectId);
     void update(Sale sale);
     void update(Sale sale, Sale oldSale);
 }
